0cfd2ae2a2c356c15811cccf72be97215bb873d0
Banushasarma <sharmabanu03@gmail.com>
2021-05-19 19:11:55 +0530

    some modifications

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,43 +1,34 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"],
-  };
-
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-    console.log("Increment Clicked", this.state.count);
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCounter()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-small m-2"
+        >
+          Delete
+        </button>
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 bg-";
-    classes += this.state.count === 0 ? "primary" : "warning";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCounter() {
-    const { count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? "Zero" : count;
   }
 }
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -14,46 +14,44 @@ class Movies extends Component {
   };
 
   render() {
-    if (this.state.movies.length === 0)
-      return <p>There no movie records in database.</p>;
-    else
-      return (
-        <div>
-          <p>Showing {this.state.movies.length} movies in the database</p>
-          <table className="table table-striped table-hover">
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Genere</th>
-                <th>Stock</th>
-                <th>Rate</th>
-                <th></th>
+    const { length: count } = this.state.movies;
+    if (count === 0) return <p>There no movie records in database.</p>;
+
+    return (
+      <React.Fragment>
+        <p>Showing {count} movies in the database</p>
+        <table className="table table-striped table-hover">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genere</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => this.handleDelete(movie._id)}
+                  >
+                    Delete
+                  </button>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {this.state.movies.map((movie) => {
-                return (
-                  <tr key={movie._id}>
-                    <td>{movie.title}</td>
-                    <td>{movie.genre.name}</td>
-                    <td>{movie.numberInStock}</td>
-                    <td>{movie.dailyRentalRate}</td>
-                    <td>
-                      <button
-                        className="btn btn-danger"
-                        onClick={() => this.handleDelete(movie._id)}
-                      >
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                );
-              })}
-              <tr></tr>
-            </tbody>
-          </table>
-        </div>
-      );
+            ))}
+            <tr></tr>
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
   }
 }
 
